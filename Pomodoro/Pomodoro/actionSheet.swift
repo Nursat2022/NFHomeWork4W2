2c371afbc3edb28abcdb8cc2266847ecbc7eb9c8
Nursat2022 <nursatsakyshev2003@gmail.com>
2023-04-08 06:32:24 +0600

    Division into structs

--- Pomodoro/Pomodoro/actionSheet.swift
+++ Pomodoro/Pomodoro/actionSheet.swift
@@ -10,152 +10,105 @@ import SwiftUI
 struct actionSheet: View {
     var progress: Double
     @State private var selectedTab = 1
+    @State private var isPlaying = false
     
     var body: some View {
         TabView {
-            
-            ZStack {
+            ZStack(alignment: .top) {
                 Image("BG")
                     .padding(.top, 36)
                 
                 VStack(spacing: 53) {
-                    HStack {
-                        Image("Pencil")
-                        
-                        Text("Focus Category")
-                            .foregroundColor(.white)
-                            .fontWeight(.semibold)
-                    }
-                    .frame(width: 170, height: 36)
-                    .background(Color(red: 1.0, green: 1.0, blue: 1.0, opacity: 0.3))
-                    .cornerRadius(20)
-//                    .padding(.top, 164)
+                    focusCategory()
                     
-                    ZStack {
-                        Circle()
-                            .strokeBorder(Color.white.opacity(0.3), lineWidth: 6)
-                            .frame(width: 248, height: 248)
-                        
-                        Circle()
-                            .trim(from: 0.0, to: CGFloat(min(self.progress, 1.0)))
-                            .stroke(style: StrokeStyle(lineWidth: 6, lineCap: .round, lineJoin: .round))
-                            .foregroundColor(.white)
-                            .rotationEffect(Angle(degrees: -90))
-                            .frame(width: 243, height: 243)
-                        
-                        VStack {
-                            Text("24:32")
-                                .font(.system(size: 44))
-                                .foregroundColor(.white)
-                                .fontWeight(.bold)
-                            Text("Focus on your task")
-                                .foregroundColor(.white)
-                                .fontWeight(.semibold)
-                        }
-                    }
+                    Clock(progress: progress, time: "24:32")
                     
-                    Spacer()
+                    Buttons(isPlaying: isPlaying)
+                    
+//                    Spacer()
                 }
                 .padding(.top, 164)
                 
                 Color.black.opacity(0.2)
                 
-                VStack {
-                    Spacer()
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(.white)
-                            .frame(height: 362)
-                        
-                        VStack {
-
-                            HStack {
-                                Spacer()
-                                Text("Focus Category")
-                                    .fontWeight(.semibold)
-                                    .padding(.leading, 35)
-                                Spacer()
-                                Button(action: {}){
-                                    Image("xmark")
-                                        .padding(.trailing, 22)
-                                }
-                            }
-                            .padding(.top, 20)
-                            .padding(.bottom, 40)
-                            
-                            VStack(spacing: 20) {
-                                HStack(spacing: 14) {
-                                    
-                                    Button(action: {}) {
-                                        Text("Work")
-                                            .foregroundColor(.black)
-                                    }
-                                    .frame(width: 170, height: 62)
-                                    .background(Color(red: 234/255, green: 234/255, blue: 234/255))
-                                    .cornerRadius(16)
-                                    .fontWeight(.semibold)
-                                    
-                                    
-                                    Button(action: {}) {
-                                        Text("Study")
-                                            .foregroundColor(.black)
-                                    }
-                                    .frame(width: 170, height: 62)
-                                    .background(Color(red: 234/255, green: 234/255, blue: 234/255))
-                                    .cornerRadius(16)
-                                    .fontWeight(.semibold)
-                                }
-                                
-                                
-                                HStack(spacing: 14) {
-                                    Button(action: {}) {
-                                        Text("Workout")
-                                    }
-                                    .foregroundColor(.white)
-                                    .frame(width: 170, height: 62)
-                                    .background(Color(red: 47/255, green: 47/255, blue: 51/255))
-                                    .cornerRadius(16)
-                                    .fontWeight(.semibold)
-                        
-                                    Button(action: {}) {
-                                        Text("Reading")
-                                            .foregroundColor(.black)
-                                    }
-                                    .frame(width: 170, height: 62)
-                                    .background(Color(red: 234/255, green: 234/255, blue: 234/255))
-                                    .cornerRadius(16)
-                                    .fontWeight(.semibold)
-                                }
-                                
-                                HStack(spacing: 14) {
-                                    Button(action: {}) {
-                                        Text("Meditation")
-                                            .foregroundColor(.black)
-                                    }
-                                        .frame(width: 170, height: 62)
-                                        .background(Color(red: 234/255, green: 234/255, blue: 234/255))
-                                        .cornerRadius(16)
-                                        .fontWeight(.semibold)
-                                        
-                                    
-                                    Button(action: {}) {
-                                        Text("Others")
-                                            .foregroundColor(.black)
-                                    }
-                                        .frame(width: 170, height: 62)
-                                        .background(Color(red: 234/255, green: 234/255, blue: 234/255))
-                                        .cornerRadius(16)
-                                        .fontWeight(.semibold)
-                                }
-                            }
-                        }
-                        .padding(.bottom, 58)
-                    }
-                }
+                Pomodoro.sheet()
+                
             }
-            .edgesIgnoringSafeArea(.all)
         }
         .tint(.white)
+    }
+}
+
+struct sheet: View {
+    var body: some View {
+       VStack {
+            Spacer()
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.white)
+                    .frame(height: 362)
+                
+                VStack {
+                  head
+                    
+                    VStack(spacing: 20) {
+
+                        HStack(spacing: 14) {
+                          sheetButton(text: "Work", backgroundColor: Color(red: 234/255, green: 234/255, blue: 234/255))
+                            
+                            sheetButton(text: "Study", backgroundColor: Color(red: 234/255, green: 234/255, blue: 234/255))
+                        }
+                        
+                        HStack(spacing: 14) {
+                            sheetButton(text: "Wokout", backgroundColor: Color(red: 47/255, green: 47/255, blue: 51/255), textColor: .white)
+                
+                            sheetButton(text: "Reading", backgroundColor: Color(red: 234/255, green: 234/255, blue: 234/255))
+                        }
+                        
+                        HStack(spacing: 14) {
+                            sheetButton(text: "Meditation", backgroundColor: Color(red: 234/255, green: 234/255, blue: 234/255))
+                                
+                            
+                            sheetButton(text: "Others", backgroundColor: Color(red: 234/255, green: 234/255, blue: 234/255))
+                        }
+                    }
+                }
+                .padding(.bottom, 58)
+            }
+        }
+    }
+    
+    var head: some View {
+        HStack {
+            Spacer()
+            Text("Focus Category")
+                .fontWeight(.semibold)
+                .padding(.leading, 35)
+            Spacer()
+            Button(action: {}){
+                Image("xmark")
+                    .padding(.trailing, 22)
+            }
+        }
+        .padding(.top, 20)
+        .padding(.bottom, 40)
+    }
+}
+
+struct sheetButton: View {
+    var text: String
+    var backgroundColor: Color
+    var textColor: Color = .black
+    
+    var body: some View {
+        Button(action: {}) {
+            Text(text)
+                .foregroundColor(textColor)
+                .frame(width: 170, height: 62)
+        }
+        .background(backgroundColor)
+        .cornerRadius(16)
+        .fontWeight(.semibold)
     }
 }
 
